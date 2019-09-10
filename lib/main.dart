fc5d78c8f8838d247fa90e3fc968920738e7b17f
lesiak <lesiak@outlook.com>
2019-09-10 10:31:29 +0200

    Changed packaging

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:music_data_display/trackswidget.dart';
+import './src/screens/homepage.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,25 +17,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TracksWidget(),
-          ],
-        ),
-      ),
-    );
-  }
-}
--- lib/src/screens/trackswidget.dart
+++ lib/src/screens/trackswidget.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'track.dart';
+import '../model/track.dart';
 
 class TracksWidget extends StatefulWidget {
   @override
